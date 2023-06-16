72b68ca972c0450ccb39b67dd5268f12e6158548
Faranosh Amini <faranosh.amini4@gmail.com>
2023-06-16 23:42:45 +0430

    Fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
-    def index
-        @user = User.find(params[:user_id])
-        @posts = Post.where(author: @user)
-      end
-      def show
-        @user = User.find(params[:user_id])
-        @post = Post.find(params[:id])
-        @comments = Comment.where(post: @post)
-      end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author: @user)
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    @comments = Comment.where(post: @post)
+  end
 end
-  
